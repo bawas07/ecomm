00d6c95d3e479aa9d55062c7c776b6ace6496920
bawas07 <bayu.waskita.07@gmail.com>
2018-07-13 17:29:22 +0700

    adding script to add value with user and admin

--- src/model/injectAdmin.js
+++ src/model/injectAdmin.js
@@ -3,6 +3,7 @@ var User = require('./user.model');
 const bcrypt = require('bcrypt')
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/ecommerce')
+const message = require('./injectMessage')
 
 bcrypt.hash('123', 10, function(err, hash){
     if (err) {
@@ -26,3 +27,27 @@ bcrypt.hash('123', 10, function(err, hash){
         console.log('pass = 123')
       })
 })
+
+bcrypt.hash('123', 10, function(err, hash){
+    if (err) {
+        console.log(err)
+        res.status(500).json({
+            error:err
+        })
+    }
+    const user = User({
+        _id: new mongoose.Types.ObjectId(),
+        email: 'user@user.com',
+        password: hash,
+        isAdmin: true
+    })
+    user.save(function(err) {
+        if (err)  {
+            console.log(err)};
+      
+        console.log('User created')
+        console.log('email = user@user.com')
+        console.log('pass = 123')
+      })
+})
+message.message()
--- src/model/injectMessage.js
+++ src/model/injectMessage.js
@@ -1,14 +1,19 @@
 const Message = require('./message.model')
 const mongoose = require('mongoose')
 
-const message1 = new Message({
-    _id: new mongoose.Types.ObjectId(),
-    name: 'payment',
-    description: 'Your payment is successful',
-})
-message1.save(function(err, data){
-    if (err){
-        console.log(err)
-    }
-    console.log(data)
-})
+exports.message = () => {
+    const message1 = new Message({
+        _id: new mongoose.Types.ObjectId(),
+        name: 'payment',
+        description: 'Your payment is successful',
+    })
+    message1.save(function(err, data){
+        if (err){
+            console.log(err)
+        }
+        console.log(data)
+    })
+}
+
+
+
